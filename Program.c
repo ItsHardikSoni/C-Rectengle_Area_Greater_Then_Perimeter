5507c8069cceff817911f4ecf12c014c56e442ca
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-04 11:27:40 +0530

    Update and rename Area of Rectangle is Greater its Perimeter to Program.c

--- Program.c
+++ Program.c
@@ -1,4 +1,8 @@
-/* Define Area of a Rectangle is Greater Then Its Perimeter */
+// Given the length and breadth of a rectangle,
+// write a program to find whether the area of the rectangle is greater than its perimeter. For example,
+// the area of the rectangle with length = 5 and breadth = 4 is greater than its perimeter.
+
+
 #include<stdio.h>
 #include<conio.h>
 int main()
